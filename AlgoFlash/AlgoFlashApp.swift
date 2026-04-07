37544aa831d3164b9e8b563f1f890b6024374664
Emon <mdahsanulislamemon@gmail.com>
2026-04-07 15:09:03 +0600

    initialization of application

---
+++ AlgoFlash/AlgoFlashApp.swift
@@ -0,0 +1,16 @@
+//
+//  AlgoFlashApp.swift
+//  AlgoFlash
+//
+//
+
+import SwiftUI
+
+@main
+struct AlgoFlashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlgoFlash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlgoFlash
+//
+//  Created by Md. Shifat Hasan on 3/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
