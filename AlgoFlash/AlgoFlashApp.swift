9e070ba74c1286a209887934db47e407b08d68bb
aCoderFromAnotherWorld <128140160+aCoderFromAnotherWorld@users.noreply.github.com>
2026-04-08 01:08:20 +0600

    status back to the commit 6d530cf:plan added and unwanted files are removed

--- AlgoFlash/AlgoFlashApp.swift
+++ AlgoFlash/AlgoFlashApp.swift
@@ -2,14 +2,26 @@ import SwiftUI
 import UIKit
 import FirebaseCore
 
+
+#if canImport(FirebaseCore)
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
-        if FirebaseApp.app() == nil {
-            FirebaseApp.configure()
+        guard FirebaseApp.app() == nil else {
+            return true
         }
+
+        guard
+            let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
+            let options = FirebaseOptions(contentsOfFile: filePath)
+        else {
+            assertionFailure("Missing GoogleService-Info.plist in the AlgoFlash target.")
+            return true
+        }
+
+        FirebaseApp.configure(options: options)
         return true
     }
 }
@@ -17,6 +29,7 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
 @main
 struct AlgoFlashApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
 
     var body: some Scene {
         WindowGroup {
