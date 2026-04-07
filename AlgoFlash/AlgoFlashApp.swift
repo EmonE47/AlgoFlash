49feba8c9a880fb7e5d319e8a229d1d6c03b79a8
aCoderFromAnotherWorld <128140160+aCoderFromAnotherWorld@users.noreply.github.com>
2026-04-07 23:55:00 +0600

    refactor: simplify Firebase initialization and remove unnecessary conditional compilation

--- AlgoFlash/AlgoFlashApp.swift
+++ AlgoFlash/AlgoFlashApp.swift
@@ -1,41 +1,22 @@
 import SwiftUI
-
-#if canImport(UIKit)
 import UIKit
-#endif
-#if canImport(FirebaseCore)
 import FirebaseCore
-#endif
 
-#if canImport(FirebaseCore)
 final class AppDelegate: NSObject, UIApplicationDelegate {
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
     ) -> Bool {
-        guard FirebaseApp.app() == nil else {
-            return true
+        if FirebaseApp.app() == nil {
+            FirebaseApp.configure()
         }
-
-        guard
-            let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist"),
-            let options = FirebaseOptions(contentsOfFile: filePath)
-        else {
-            assertionFailure("Missing GoogleService-Info.plist in the AlgoFlash target.")
-            return true
-        }
-
-        FirebaseApp.configure(options: options)
         return true
     }
 }
-#endif
 
 @main
 struct AlgoFlashApp: App {
-    #if canImport(FirebaseCore)
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
-    #endif
 
     var body: some Scene {
         WindowGroup {
