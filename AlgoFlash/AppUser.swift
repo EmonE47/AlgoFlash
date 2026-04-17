3f66ecb36a34b01c3e638b7238f5ce80c095e957
Emon <mdahsanulislamemon@gmail.com>
2026-04-18 01:32:09 +0600

    Add role and favourites fields to AppUser model

--- AlgoFlash/AppUser.swift
+++ AlgoFlash/AppUser.swift
@@ -2,8 +2,15 @@ import Foundation
 
 struct AppUser: Codable {
     let id: String
-    let fullName: String
     let email: String
+    let fullName: String
+    let role: String  // "admin" or "user"
     var score: Int
     var quizzesTaken: Int
+    var favourites: [Int]
+}
+
+enum UserRole: String {
+    case admin = "admin"
+    case user = "user"
 }
